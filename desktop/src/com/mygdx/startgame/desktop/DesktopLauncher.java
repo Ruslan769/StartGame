a3d8cd9eb213f918d4b1b9bb577aa450f56e7f12
Ruslan <R76987R@yandex.ru>
2018-09-14 16:51:30 +0300

    Initial commit

---
+++ desktop/src/com/mygdx/startgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.startgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.startgame.MyStartgame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyStartgame(), config);
+	}
+}
